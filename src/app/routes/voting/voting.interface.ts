919043b4041e6ee8d1c1655d61516fc772360c0a
b0bbobovich <91199600+b0bbobovich@users.noreply.github.com>
2022-08-18 19:20:49 +0300

    Auto-generated commit [ci skip]

--- src/app/routes/voting/voting.interface.ts
+++
@@ -1,6 +0,0 @@
-export class LoggedData {
-    imageID!: string;
-    action!: string;
-    place!: string;
-    currentTime!: string;
-}
--- src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  apiKey: 'f4be14b0-337c-476e-9dd2-5baa65590e13',
-  apiURL: 'https://api.thecatapi.com/v1'
-};
